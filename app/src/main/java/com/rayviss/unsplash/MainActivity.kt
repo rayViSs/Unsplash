4f001de1c998b8322d7d0bb10499a573f3452cca
Vladislav <sobennikov.02@mail.ru>
2023-06-28 23:05:02 +0300

    Initial commit

---
+++ app/src/main/java/com/rayviss/unsplash/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.rayviss.unsplash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rayviss.unsplash.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/rayviss/unsplash/ui/main/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.rayviss.unsplash.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+}
